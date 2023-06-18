c2f5c657ae15e145f7a673b783043057cfc5b573
Lycoris62 <wodbs7758@naver.com>
2023-06-18 23:44:04 +0900

    feat: @SendTo 어노테이션으로 수정

--- src/main/java/lycoris62/socialLoginApp/rps/stomp/StompChatController.java
+++ src/main/java/lycoris62/socialLoginApp/rps/stomp/StompChatController.java
@@ -2,8 +2,9 @@ package lycoris62.socialLoginApp.rps.stomp;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -11,20 +12,16 @@ import org.springframework.stereotype.Controller;
 @Slf4j
 public class StompChatController {
 
-    private final SimpMessagingTemplate template;
-
-    @MessageMapping("/chat/enter")
-    public void enter(ChatMessageDto message) {
+    @MessageMapping("/chat/enter/{roomId}")
+    @SendTo("/sub/chat/room/{roomId}")
+    public ChatMessageDto enter(@DestinationVariable String roomId, ChatMessageDto message) {
         message.setMessage(message.getWriter() + "님이 입장하셨습니다.");
-        log.info("입장={}, msg={}", message.getWriter(), message.getMessage());
-        log.info("전송url={}", "/sub/chat/room/" + message.getRoomId());
-        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        return message;
     }
 
-    @MessageMapping("/chat/message")
-    public void message(ChatMessageDto message) {
-        log.info("전송={}, msg={}", message.getWriter(), message.getMessage());
-        log.info("전송url={}", "/sub/chat/room/" + message.getRoomId());
-        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    @MessageMapping("/chat/message/{roomId}")
+    @SendTo("/sub/chat/room/{roomId}")
+    public ChatMessageDto message(@DestinationVariable String roomId, ChatMessageDto message) {
+        return message;
     }
 }
