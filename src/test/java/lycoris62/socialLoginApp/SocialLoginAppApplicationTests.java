4d755d26941f1c28d8f2096fcf76d2144a9884cd
barami62 <wodbs7758@naver.com>
2023-06-22 21:56:04 +0900

    fix : 스프링부트 테스트 어노테이션 제거

--- src/test/java/lycoris62/socialLoginApp/SocialLoginAppApplicationTests.java
+++ src/test/java/lycoris62/socialLoginApp/SocialLoginAppApplicationTests.java
@@ -1,9 +1,7 @@
 package lycoris62.socialLoginApp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SocialLoginAppApplicationTests {
 
 	@Test
