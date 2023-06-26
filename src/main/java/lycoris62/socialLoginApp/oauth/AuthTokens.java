d3696893cde7d980eccb10f6aaa89db44c4957dc
barami62 <wodbs7758@naver.com>
2023-06-26 15:10:05 +0900

    feat: AuthToken 추가

---
+++ src/main/java/lycoris62/socialLoginApp/oauth/AuthTokens.java
@@ -0,0 +1,18 @@
+package lycoris62.socialLoginApp.oauth;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthTokens {
+    private String accessToken;
+    private String refreshToken;
+    private String grantType;
+    private Long expiresIn;
+
+    public static AuthTokens of(String accessToken, String refreshToken, String grantType, Long expiresIn) {
+        return new AuthTokens(accessToken, refreshToken, grantType, expiresIn);
+    }
+}
